cdc685dbcaa3465ba6b7090fac9a9c0dccd646d7
MrZ-0714 <zimo.zhao.0714@gmail.com>
2021-04-05 14:20:11 -0700

    Initial Commit

---
+++ I am RICH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am RICH
+//
+//  Created by Zimo Zhao on 4/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
